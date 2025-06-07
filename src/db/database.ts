f0c47822dec2323e68e53794b738e0062390d6b5
Rahib <rahibbutt@gmail.com>
2025-06-07 14:27:04 +0200

    feat: add dev command to run local dev server

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,7 +2,7 @@ import Database from 'better-sqlite3';
 import path from 'path';
 
 const dbPath = path.resolve(__dirname, '../../pokemon.db'); // ⬅ correct!
-console.log('path', dbPath)
+// console.log('path', dbPath)
 const db = new Database(dbPath);
 
 export default db;
