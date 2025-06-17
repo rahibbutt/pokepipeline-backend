1fd7579e83115421095c22c9a1dd7ab440faef53
Rahib <rahibbutt@gmail.com>
2025-06-17 20:57:45 +0200

    feat: configure typescript-eslint to use prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,17 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   {
     ignores: ["dist/**", "**.config.**"],
+  },
+  {
+    rules: {'prettier/prettier': 'error'},
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
   },
   eslint.configs.recommended,
   tseslint.configs.recommended,
